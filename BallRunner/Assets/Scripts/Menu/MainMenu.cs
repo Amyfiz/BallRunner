9a3984a67221ee9496eb6ec89c004c3788d23ae4
Amyfiz <a.solonukhina@gmail.com>
2023-03-07 20:05:46 +0300

    Text Mash Pro added + Start Menu added

---
+++ BallRunner/Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject selectLevel;
+    private bool levelsOn;
+
+    public void Start()
+    {
+        levelsOn = false;
+        selectLevel.SetActive(false);
+    }
+
+    public void StartNewGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ShowLevels()
+    {
+        if (!levelsOn)
+        {
+            selectLevel.SetActive(!selectLevel.activeSelf);
+        }
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+}
